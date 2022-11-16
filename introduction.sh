4ebeee41dc7e4ce44cc380a94e12a2213aaa92e1
“raulsouza” <raul.souza@linux.com>
2022-11-16 20:24:00 -0300

    add one more question, changed the file to save the info

--- introduction.sh
+++ introduction.sh
@@ -3,7 +3,9 @@
     # Ask the user for their name
     echo Hello, who am I talking to?
     read varname
+    echo How old Are you?
+    read varage
     echo It\'s nice to meet you $varname
-    echo $varname >> names
+    echo $varname $varage >> people
 
 
